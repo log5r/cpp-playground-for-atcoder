17ce286611061312802ca7211cd90b479dcad542
judau <judau.m.r.t@gmail.com>
2020-11-29 01:18:23 +0900

    test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+//char get_winner(char a, char b) {
+//    if(a == 'R' && b == 'S') return a;
+//    if(a == 'P' && b == 'R') return a;
+//    if(a == 'S' && b == 'P') return a;
+//    return b;
+//}
+
+int main(){
+    ios_base::sync_with_stdio(false), cin.tie(nullptr);
+
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    auto get_winner = [&](char a, char b) -> char {
+        if(a == 'R' && b == 'S') return a;
+        if(a == 'P' && b == 'R') return a;
+        if(a == 'S' && b == 'P') return a;
+        return b;
+    };
+    for(int i = 0; i < k; ++i){
+        s += s;
+        string str;
+        for(int j = 0; j < n; ++j){
+            str += get_winner(s[2 * j], s[2 * j + 1]);
+        }
+        s = str;
+        cout << s.front() << endl;
+    }
 }

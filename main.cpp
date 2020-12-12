6147277abdb53fa17617e65b8b7c771b3aa171f2
judau <judau.m.r.t@gmail.com>
2020-12-13 03:09:15 +0900

    up

--- main.cpp
+++ main.cpp
@@ -1,33 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-//char get_winner(char a, char b) {
-//    if(a == 'R' && b == 'S') return a;
-//    if(a == 'P' && b == 'R') return a;
-//    if(a == 'S' && b == 'P') return a;
-//    return b;
-//}
+#define pln(text) cout << text << endl
+#define rep(i, n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define repr(vec) for (const auto&it: vec)
+#define all(x) (x).begin(),(x).end()
+#define MERGE(v, a) v.insert(v.end(), all(a))
+#define SORT(x) sort(x.begin(), x.end())
+#define SORT_UNIQUE(v) SORT(v);v.erase(unique(v.begin(), v.end()), v.end())
+#define UPDATE_NUM(current, challenger) if (challenger > current) current = challenger
 
 int main(){
-    ios_base::sync_with_stdio(false), cin.tie(nullptr);
 
-    int n, k;
-    cin >> n >> k;
-    string s;
-    cin >> s;
-    auto get_winner = [&](char a, char b) -> char {
-        if(a == 'R' && b == 'S') return a;
-        if(a == 'P' && b == 'R') return a;
-        if(a == 'S' && b == 'P') return a;
-        return b;
-    };
-    for(int i = 0; i < k; ++i){
-        s += s;
-        string str;
-        for(int j = 0; j < n; ++j){
-            str += get_winner(s[2 * j], s[2 * j + 1]);
-        }
-        s = str;
-        cout << s.front() << endl;
-    }
 }

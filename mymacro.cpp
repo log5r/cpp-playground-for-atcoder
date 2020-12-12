3a5ea31045a7bad6bdad693bd80c5ee25d40d0c3
judau <judau.m.r.t@gmail.com>
2020-12-13 03:29:17 +0900

    up

--- mymacro.cpp
+++ mymacro.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 #define pln(text) cout << text << endl
 #define rep(i, n) for(int i=0, i##_len=(n); i<i##_len; ++i)
 #define repr(vec) for (const auto&it: vec)
@@ -10,21 +11,3 @@ using namespace std;
 #define SORT_REVERSE(x) SORT(x);reverse(all(x))
 #define SORT_UNIQUE(v) SORT(v);v.erase(unique(v.begin(), v.end()), v.end())
 #define UPDATE_NUM(current, challenger) if (challenger > current) current = challenger
-
-int main(){
-    int N; cin >> N;
-    vector<int> dl(N); rep(i, N) cin >> dl.at(i);
-    SORT(dl);
-
-    int latest_radius = 2e9;
-    int height = 0;
-    until(dl.empty()) {
-        int current_radius = dl.back();
-        if (latest_radius > current_radius) {
-            height++;
-            latest_radius = current_radius;
-        }
-        dl.pop_back();
-    }
-    pln(height);
-}

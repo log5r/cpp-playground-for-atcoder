6e44abf1ba383f7d57ad1eeb5290974a6775e2aa
judau <judau.m.r.t@gmail.com>
2021-09-15 14:02:33 +0900

    mainを整理

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
+#define _GLIBCXX_DEBUG
 #include <bits/stdc++.h>
-
 using namespace std;
 using ll = long long;
 #define rep(i, n) for(int i=0, i##_len=(n); i<i##_len; ++i)
 #define all(x) (x).begin(),(x).end()
-
 
 int main() {
 

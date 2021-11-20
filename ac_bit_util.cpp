1207f929654eb02e5d01728c4e901d875c9f323c
judau <jusk1nsnt9gi>
2021-11-20 18:16:44 +0900

    ビット全探索用の関数を追加

--- ac_bit_util.cpp
+++ ac_bit_util.cpp
@@ -4,3 +4,18 @@ using namespace std;
 // ...本当に使うんですかね
 
 #define bitbfs(tmp, bit_size) for (int tmp = 0; tmp < (1 << bit_size); tmp++)
+
+/// bitset を使わずに、考えられるビット列を全列挙
+/// \param size ビット列の長さ
+/// \return あり得るビット列のセット
+vector<string> bitt_str_list(int size) {
+    vector<string> res;
+    for (int i = 0; i < (1 << size); ++i) {
+        string buf;
+        for (int j = size - 1; j >= 0; --j) {
+            buf += (i & (1 << j)) ? '1' : '0';
+        }
+        res.push_back(buf);
+    }
+    return res;
+}
